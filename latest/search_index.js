5e8d989815a2342ec2f24ea80851461c86e05172
autodocs <autodocs>
2018-11-20 00:46:06 +0000

    build based on ed5e2a6

--- latest/search_index.js
+++ latest/search_index.js
@@ -50,31 +50,47 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#",
-    "page": "Full Traces",
-    "title": "Full Traces",
+    "page": "Generating Traces",
+    "title": "Generating Traces",
     "category": "page",
     "text": ""
 },
 
 {
     "location": "trace.html#MemSnoop.trace",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.trace",
     "category": "function",
     "text": "trace(pid; [sampletime], [iter], [filter]) -> Vector{Sample}\n\nRecord the full trace of pages accessed by an application with pid. Function will gracefully exit and return Vector{Sample} if process pid no longer exists.\n\nThe general flow of this function is as follows:\n\nSleep for sampletime.\nPause pid.\nGet the VMAs for pid, applying filter.\nRead all of the active pages.\nMark all pages as idle.\nResume `pid.\nRepeat for each element of iter.\n\nKeyword Arguments\n\nsampletime : Seconds between reading and reseting the idle page flags to determine page   activity. Default: 2\niter : Iterator to control the number of samples to take. Default behavior is to keep   sampling until monitored process terminates. Default: Run until program terminates.\nfilter : Filter to apply to process VMAs to reduce total amount of memory tracked.\n\n\n\n\n\n"
 },
 
 {
-    "location": "trace.html#Full-Traces-1",
-    "page": "Full Traces",
-    "title": "Full Traces",
+    "location": "trace.html#MemSnoop.pages-Tuple{Array{MemSnoop.Sample,1}}",
+    "page": "Generating Traces",
+    "title": "MemSnoop.pages",
+    "category": "method",
+    "text": "pages(trace::Vector{Sample}) -> Vector{UInt64}\n\nReturn a sorted vector of all pages in trace that were marked as \"active\" at least once. Pages are encoded by virtual page number.\n\n\n\n\n\n"
+},
+
+{
+    "location": "trace.html#MemSnoop.vmas-Tuple{Array{MemSnoop.Sample,1}}",
+    "page": "Generating Traces",
+    "title": "MemSnoop.vmas",
+    "category": "method",
+    "text": "vmas(trace::Vector{Sample}) -> Vector{VMA}\n\nReturn the largest sorted collection V of VMAs with the property that for any sample S in trace and for any VMA s in S, s subset v for some v in V and s cap u = emptyset for all u in V setminus v.o\n\n\n\n\n\n"
+},
+
+{
+    "location": "trace.html#Generating-Traces-1",
+    "page": "Generating Traces",
+    "title": "Generating Traces",
     "category": "section",
-    "text": "MemSnoop has the ability to record the full trace of pages accessed by an application. This is performed using traceMemSnoop.trace"
+    "text": "MemSnoop has the ability to record the full trace of pages accessed by an application. This is performed using traceMemSnoop.trace\nMemSnoop.pages(::Vector{MemSnoop.Sample})\nMemSnoop.vmas(::Vector{MemSnoop.Sample})"
 },
 
 {
     "location": "trace.html#MemSnoop.Sample",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.Sample",
     "category": "type",
     "text": "Simple container containing the list of VMAs analyzed for a sample as well as the individual pages accessed.\n\nFields\n\nvmas :: Vector{VMA} - The VMAs analyzed during this sample.\npages :: RangeVector{UInt64} - The pages that were active during this sample. Pages are   encoded by virtual page number. To get an address, multiply the page number by the   pagesize (generally 4096).\n\n\n\n\n\n"
@@ -82,31 +98,39 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#MemSnoop.isactive-Tuple{MemSnoop.Sample,Any}",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.isactive",
     "category": "method",
     "text": "isactive(sample::Sample, page) -> Bool\n\nReturn true if page was active in sample.\n\n\n\n\n\n"
 },
 
 {
-    "location": "trace.html#MemSnoop.pages",
-    "page": "Full Traces",
+    "location": "trace.html#MemSnoop.pages-Tuple{MemSnoop.Sample}",
+    "page": "Generating Traces",
     "title": "MemSnoop.pages",
-    "category": "function",
-    "text": "pages(sample::Sample) -> Set{UInt64}\n\nReturn a set of all active pages in sample.\n\n\n\n\n\npages(trace::Vector{Sample}) -> Vector{UInt64}\n\nReturn a sorted vector of all pages in trace that were marked as \"active\" at least once. Pages are encoded by virtual page number.\n\n\n\n\n\n"
+    "category": "method",
+    "text": "pages(sample::Sample) -> Set{UInt64}\n\nReturn a set of all active pages in sample.\n\n\n\n\n\n"
+},
+
+{
+    "location": "trace.html#MemSnoop.bitmap-Tuple{Array{MemSnoop.Sample,1},MemSnoop.VMA}",
+    "page": "Generating Traces",
+    "title": "MemSnoop.bitmap",
+    "category": "method",
+    "text": "bitmap(trace::Vector{Sample}, vma::VMA) -> Array{Bool, 2}\n\nReturn a bitmap B of active pages in trace with virtual addresses from vma.  B[i,j] == true if the ith address in vma in trace[j] is active.\n\n\n\n\n\n"
 },
 
 {
     "location": "trace.html#Implementation-Details-Sample-1",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "Implementation Details - Sample",
     "category": "section",
-    "text": "MemSnoop.Sample\nMemSnoop.isactive(::MemSnoop.Sample, ::Any)\nMemSnoop.pages"
+    "text": "MemSnoop.Sample\nMemSnoop.isactive(::MemSnoop.Sample, ::Any)\nMemSnoop.pages(::MemSnoop.Sample)\nMemSnoop.bitmap(::Vector{MemSnoop.Sample}, ::MemSnoop.VMA)"
 },
 
 {
     "location": "trace.html#MemSnoop.RangeVector",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.RangeVector",
     "category": "type",
     "text": "Compact representation of data of type T that is both sorted and usually occurs in contiguous ranges. For example, since groups of virtual memory pages are usually accessed together, a RangeVector can encode those more compactly than a normal vector.\n\nFields\n\nranges :: Vector{UnitRange{T} - The elements of the RangeVector, compacted into   contiguous ranges.\n\nConstructor\n\nRangeVector{T}() -> RangeVector{T}\n\nConstruct a empty RangeVector with element type T.\n\n\n\n\n\n"
@@ -114,7 +138,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#MemSnoop.lastelement",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.lastelement",
     "category": "function",
     "text": "lastelement(R::RangeVector{T}) -> T\n\nReturn the last element of the last range of R.\n\n\n\n\n\n"
@@ -122,7 +146,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#Base.push!-Union{Tuple{T}, Tuple{RangeVector{T},T}} where T",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "Base.push!",
     "category": "method",
     "text": "push!(R::RangeVector{T}, x::T)\n\nAdd x to the end of R, merging x into the final range if appropriate.\n\n\n\n\n\n"
@@ -130,7 +154,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#MemSnoop.insorted",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "MemSnoop.insorted",
     "category": "function",
     "text": "insorted(R::RangeVector, x) -> Bool\n\nPerform an efficient search of R for item x, assuming the ranges in R are sorted and non-overlapping.\n\n\n\n\n\n"
@@ -138,10 +162,50 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "trace.html#Implementation-Details-RangeVector-1",
-    "page": "Full Traces",
+    "page": "Generating Traces",
     "title": "Implementation Details - RangeVector",
     "category": "section",
     "text": "Since pages are generally accessed sequentially, the record of active pages is encoded as a MemSnoop.RangeVector that compresses contiguous runs of accesses. Note that  there is an implicit assumption that the VMAs are ordered, which should be the case since  /prod/pid/maps orderes VMAs.MemSnoop.RangeVector\nMemSnoop.lastelement\npush!(::MemSnoop.RangeVector{T}, x::T) where T\nMemSnoop.insorted"
+},
+
+{
+    "location": "thoughts.html#",
+    "page": "Thoughts",
+    "title": "Thoughts",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "thoughts.html#Thoughts-1",
+    "page": "Thoughts",
+    "title": "Thoughts",
+    "category": "section",
+    "text": "DISCLAIMER: This is me basically sketching some ideas I had so I can put them in a more coherent form later. This is not meant yet to be understandable by others. Proceed with  caution :DOriginally, I had thought that sampling at a smaller time interval would yield strictly better results, but I\'m not so sure anymore. Here is an outline of my though process so far.Definition: Let t mathbbN to mathbbR^+ be a time sequence if t_0  0  and t_i+1  t_i.Definition: Define a sample S asS_t_i =  k  textPage k was active between t_i-1 and t_i be the set of pages of active pages on the time interval (t_i-1 t_i where t is a time sequence. As a syntactical choice, let S(t 0) = emptyset.Definition Define a trace T = (S_t_0 S_t_1 ldots S_t_n) be an ordered collection of samples.What we would like to show is that if we have two time sequences t and t^prime where t_i+1 - t_i leq t^prime _j+1 - t^prime _j for all index pairs i and j, (in other words, time sequence t has a smaller time interval than t^prime, then t is somehow a better approximation of the ground truth of reuse distance. To do  this, we need to define what we mean by reuse distance in the sense of discrete samples.Let t_0 and t_1 denote the times between subsequent accesses to some page k.  Furthermore, suppose the time sequence t denoting our samples times is such that for some index i, t_i-1  t_0 leq t_i and t_i+1  t_1 leq t_i+2. That is, the  sampling periods look something like what we have below:(Image: )We have to be conservative and say that the reuse distance for page k at this time interval isd(t i+2 k) = left S_t_i cup S_t_i+1 cup S_t_i+2 setminus k rightNow, we assume that page k is not accessed during S_t_i+. One source of error comes from over counting unique accesses on either side of the samples containing the page of interest.If subsequent accesses to page k are recorded in S_t_i and S_t_j, then a bound on the error in the reuse distance calculation isE =  S_t_i cup S_t_j setminus k  =  S_t_i cup S_t_j  - 1(Side note for further clarification, this works if we think of an oracle sampling function t^* where S_t^* _i = 1 for all valid i. I.E., a perfect trace).This is not taking into acount sub-sampling-frame rehits. "
+},
+
+{
+    "location": "thoughts.html#A-pathological-example-1",
+    "page": "Thoughts",
+    "title": "A pathological example",
+    "category": "section",
+    "text": "Consider the following pathological example:(Image: )Here, we have two sampling periods, with the exact sequence of page hits shown by the  vertical bars. The exact trace histogram is shown on the left, with the one generated by the approximation shown on the right. The dashed bar is the approximated histogram is all  subpage accesses are recorded."
+},
+
+{
+    "location": "thoughts.html#Error-Terms-1",
+    "page": "Thoughts",
+    "title": "Error Terms",
+    "category": "section",
+    "text": "Let t_i and t_j be sample times for subsequent accesses to page k. If we use the pessimistic distance formula, than the error in the reuse distance is S_t_i cup S_t_j  - 1Let r_{t_i} be the number of repeated accesses in S_{t_i}. Then the error here isr_t_i (S_t_i - 1)"
+},
+
+{
+    "location": "thoughts.html#Idea-1",
+    "page": "Thoughts",
+    "title": "Idea",
+    "category": "section",
+    "text": "We can actually generate a lower bound AND an upper bound for reuse distance via sampling."
 },
 
 ]}
